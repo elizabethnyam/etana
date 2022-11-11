ef50d30254637e6ba7665e5740eefdb6f5942a5f
Elizabeth Nyamwange <artsygirlen@gmail.com>
2022-11-11 15:46:25 -0600

    Add files via upload

---
+++ workspaces/homepage/README.md
@@ -0,0 +1,3 @@
+# Homepage
+
+This workspace contains the homepage for the [idbox.online](https://idbox.online) page.
---
+++ workspaces/homepage/gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Identity Box',
+    editBaseUrl: 'https://github.com/identity-box/identity-box/blob/master'
+  },
+  plugins: [
+    'gatsby-plugin-provide-react',
+    '@confluenza/gatsby-theme-confluenza',
+    'gatsby-plugin-root-import'
+  ]
+}
